fd88f32f5b87464d286c4fa251a5c2c0ee95a38b
zuhayrdbse <zuhayrdanish2@gmail.com>
2024-09-17 01:12:50 +0530

    Files added

--- app.js
+++ app.js
@@ -9,17 +9,27 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+// Handle socket connections
 io.on("connection", function (socket) {
+  console.log("New user connected");
+
   socket.on("send-location", function (data) {
     io.emit("receive-location", { id: socket.id, ...data });
   });
+
   socket.on("disconnect", function () {
+    console.log("User disconnected");
     io.emit("user-disconnected", socket.id);
   });
 });
 
+// Render the index page
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-server.listen(3000);
+// Listen on the port specified by the environment variable or default to 3000
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
